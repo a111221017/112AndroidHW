7bdd32b45d353d346d092ea050b8fdcd2a05d1b6
a111221017 <162554551+a111221017@users.noreply.github.com>
2024-03-26 22:35:46 +0800

    Add files via upload

---
+++ LoginUI/app/src/main/java/com/example/loginui/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.loginui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextUsername, editTextPassword;
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = editTextUsername.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+
+                if (username.equals("admin") && password.equals("admin")) {
+                    // 登入成功
+                    Toast.makeText(MainActivity.this, "登入成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    // 登入失敗
+                    Toast.makeText(MainActivity.this, "登入失敗", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
